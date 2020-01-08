b4a80491bf47ef71720e5491d5ff3c921156def4
burritoOverflow <rschuman@ramapo.edu>
2020-01-08 16:43:43 -0500

    form refactored to seperate component

--- src/components/Contacts/AddContact.js
+++ src/components/Contacts/AddContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Consumer } from "../../Context";
+import TextInputGroup from "../layout/TextInputGroup";
 import uuid from "uuid";
 
 class AddContact extends Component {
@@ -45,42 +46,29 @@ class AddContact extends Component {
               <div className="card-header">Add Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input
-                      type="text"
-                      name="name"
-                      className="form-control form-control-lg"
-                      placeholder="Enter name"
-                      value={name}
-                      onChange={this.handleChange}
-                      required
-                    ></input>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      name="email"
-                      className="form-control form-control-lg"
-                      placeholder="Enter e-mail"
-                      value={email}
-                      onChange={this.handleChange}
-                      required
-                    ></input>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="phone">Phone</label>
-                    <input
-                      type="tel"
-                      name="phone"
-                      className="form-control form-control-lg"
-                      placeholder="Enter phone number"
-                      value={phone}
-                      onChange={this.handleChange}
-                      required
-                    ></input>
-                  </div>
+                  <TextInputGroup
+                    label="Name"
+                    name="name"
+                    placeholder="Enter name"
+                    value={name}
+                    onChange={this.handleChange}
+                  />
+                  <TextInputGroup
+                    label="Email"
+                    name="email"
+                    type="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={this.handleChange}
+                  />
+                  <TextInputGroup
+                    label="Phone"
+                    name="phone"
+                    type="tel"
+                    placeholder="Enter phone number"
+                    value={phone}
+                    onChange={this.handleChange}
+                  />
                   <input
                     type="submit"
                     value="Add Contact"
