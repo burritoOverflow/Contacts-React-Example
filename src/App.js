18b25f949b02ef924e82c7ef8d2b0dcf5602ac40
burritoOverflow <rschuman@ramapo.edu>
2020-01-08 00:31:56 -0500

    bootstrap additions

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import Contact from "./components/Contact";
 import Header from "./components/Header";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
     <div className="App">
       <Header branding="Contact Manager" />
-      <h1>Heello world</h1>
       <Contact name="John Doe" email="Jdoe@poop.org" phone="21231-3131" />
       <Contact name="Peter Doe" email="Jdoe@poop.org" phone="21231-3131" />
     </div>
