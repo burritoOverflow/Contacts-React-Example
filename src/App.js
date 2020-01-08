740a86228c6aa0c5ae0cf3a5f3d0b3fb054d9027
burritoOverflow <rschuman@ramapo.edu>
2020-01-08 00:09:00 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header branding="Contact Manager" />
+      <h1>Heello world</h1>
+      <Contact name="John Doe" email="Jdoe@poop.org" phone="21231-3131" />
+      <Contact name="Peter Doe" email="Jdoe@poop.org" phone="21231-3131" />
     </div>
   );
 }
