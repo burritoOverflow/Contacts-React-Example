89b6182548c3db32916005024a39d988515da0c8
burritoOverflow <rschuman@ramapo.edu>
2020-01-08 00:49:38 -0500

    cleaner refactoring

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -2,42 +2,35 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "Jdoe@website.org",
-          phone: "123-456-9011"
-        },
-        {
-          id: 2,
-          name: "Peter Piper",
-          email: "pp@website.org",
-          phone: "555-555-1213"
-        },
-        {
-          id: 3,
-          name: "Tyler Durden",
-          email: "projectmayhem@website.org",
-          phone: "867-5309"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "Jdoe@website.org",
+        phone: "123-456-9011"
+      },
+      {
+        id: 2,
+        name: "Peter Piper",
+        email: "pp@website.org",
+        phone: "555-555-1213"
+      },
+      {
+        id: 3,
+        name: "Tyler Durden",
+        email: "projectmayhem@website.org",
+        phone: "867-5309"
+      }
+    ]
+  };
+
   render() {
     const { contacts } = this.state;
     return (
       <div>
-        {contacts.map((contact, i) => (
-          <Contact
-            key={i}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+        {contacts.map(contact => (
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
