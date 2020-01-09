c035c7b7ec79155167671d513c34d61eb4c858cb
burritoOverflow <rschuman@ramapo.edu>
2020-01-09 02:25:36 -0500

    Updates

---
+++ precache-manifest.661384fd9ff13714e6fb3b49b62db6d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4192a91c6da28ffaa08eab1f8bbfb535",
+    "url": "/customersapp/index.html"
+  },
+  {
+    "revision": "ab00f8c9cc21a36d308f",
+    "url": "/customersapp/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ab00f8c9cc21a36d308f",
+    "url": "/customersapp/static/js/2.91a59d16.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/customersapp/static/js/2.91a59d16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69911e91834924718add",
+    "url": "/customersapp/static/js/main.65229926.chunk.js"
+  },
+  {
+    "revision": "177ca95803e89a535bb9",
+    "url": "/customersapp/static/js/runtime-main.81869b7b.js"
+  }
+]);
